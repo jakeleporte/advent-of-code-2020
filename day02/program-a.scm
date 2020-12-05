3866010bae77b2f5adab900a1a5496c48992e206
Jake Leporte <jleporte@nd.edu>
2020-12-04 21:52:02 -0500

    day02: minor refactor and rename

--- day02/program-a.scm
+++ day02/program-a.scm
@@ -22,19 +22,7 @@ itself."
   "Check that a password contains between MIN and MAX
 numbers of CHAR, inclusive"
   (let ((count (string-count pass char)))
-    (if (and (>= count min) (<= count max))
-	#t
-	#f)))
-
-(define (verify-password-b char pos1 pos2 pass)
-  "Checks that a password contains char in POS1 or POS2"
-  ;; Given indices are 1-referenced; change them to 0-referenced
-  (define pos-a (1- pos1))
-  (define pos-b (1- pos2))
-  (let ((in-pos-a (char=? (string-ref pass pos-a) char))
-	(in-pos-b (char=? (string-ref pass pos-b) char)))
-    ;; Check for xor- not the same boolean value
-    (not (eq? in-pos-a in-pos-b))))
+    (and (>= count min) (<= count max))))
 
 (define (main)
   (let loop ((pass-list (read-password-line))
@@ -46,7 +34,7 @@ numbers of CHAR, inclusive"
 	      (max (caddr pass-list))
 	      (pass (cadddr pass-list)))
 	  ;; Verify the password and loop
-	  (if (verify-password-b char min max pass)
+	  (if (verify-password-a char min max pass)
 	      (loop (read-password-line) (1+ valid-pass-count))
 	      (loop (read-password-line) valid-pass-count)))
 	(display valid-pass-count))))
--- day02/program-b.scm
+++ day02/program-b.scm
@@ -3,22 +3,30 @@
 
 (use-modules (ice-9 rdelim) (ice-9 regex))
 
-(define %password-regex
+(define password-regex
   (make-regexp
    "([[:digit:]]+)-([[:digit:]]+) ([[:alpha:]]): ([[:alpha:]]+)"))
+
+(define (verify-password-b char pos-1 pos-2 pass)
+  "Checks that a password contains char in POS1 or POS2"
+  ;; Given indices are 1-referenced; change them to 0-referenced
+  (let ((in-pos-1 (char=? (string-ref pass (1- pos-1)) char))
+	(in-pos-2 (char=? (string-ref pass (1- pos-2)) char)))
+    ;; Check for xor- not the same boolean value
+    (not (eq? in-pos-1 in-pos-2))))
 
 (define (main)
   (let loop ((line (read-line))
 	     (valid 0))
     ;; Loop until EOF
     (if (not (eof-object? line))
-	(let* ((pass-match (regexp-exec %password-regex line))
+	(let* ((pass-match (regexp-exec password-regex line))
 	       (min (string->number (match:substring pass-match 1)))
 	       (max (string->number (match:substring pass-match 2)))
 	       (char (string-ref (match:substring pass-match 3) 0))
 	       (pass (match:substring pass-match 4))
 	       (count (string-count pass char)))
-	  (if (and (>= count min) (<= count max))
+	  (if (verify-password-b char min max pass)
 	      (loop (read-line) (1+ valid))
 	      (loop (read-line) valid)))
 	(begin (display valid) (newline)))))
