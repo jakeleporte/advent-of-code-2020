a8f1849202bba1521c0e520ba994f481e345f8f5
Jake Leporte <jleporte@nd.edu>
2020-12-06 17:17:53 -0500

    day06: use char-set

--- day06/program-a.scm
+++ day06/program-a.scm
@@ -7,17 +7,17 @@
   (define first-line (read-line))
   (if (eof-object? first-line) #f
       (let loop ((line first-line)
-		 (letters '()))
+		 (letters char-set:empty))
 	(if (or (eof-object? line) (string-null? line))
 	    letters
 	    (loop (read-line)
-		  (lset-union char=? letters (string->list line)))))))
+		  (char-set-union letters (string->char-set line)))))))
 
 (define (main)
   (let loop ((letters (process-customs-form-a))
 	     (count 0))
     (if letters
-      (loop (process-customs-form-a) (+ count (length letters)))
+      (loop (process-customs-form-a) (+ count (char-set-size letters)))
       (begin (display count) (newline)))))
 
 (main)
--- day06/program-b.scm
+++ day06/program-b.scm
@@ -9,17 +9,17 @@
   (define first-line (read-line))
   (if (eof-object? first-line) #f
       (let loop ((line first-line)
-		 (letters (string->list "abcdefghijklmnopqrstuvwxyz")))
+		 (letters char-set:lower-case))
 	(if (or (eof-object? line) (string-null? line))
 	    letters
 	    (loop (read-line)
-		  (lset-intersection char=? letters (string->list line)))))))
+		  (char-set-intersection letters (string->char-set line)))))))
 
 (define (main)
   (let loop ((letters (process-customs-form-b))
 	     (count 0))
     (if letters
-      (loop (process-customs-form-b) (+ count (length letters)))
+      (loop (process-customs-form-b) (+ count (char-set-size letters)))
       (begin (display count) (newline)))))
 
 (main)
