5cd93cd9c367e098d44f3b5db71fe12559119c98
Jake Leporte <jleporte@nd.edu>
2020-12-05 22:04:38 -0500

    day01: scheme solution

---
+++ day01/program-a.scm
@@ -0,0 +1,33 @@
+#!/usr/bin/env guile
+!#
+
+;;; Inneficient (O(n^2)) solution
+
+(use-modules (ice-9 rdelim) (srfi srfi-1))
+
+(define (read-numbers)
+  "Read in a list of numbers from stdin"
+  (do ((line (read-line) (read-line))
+       (numbers '() (append numbers (list (string->number line)))))
+      ((eof-object? line) numbers)))
+
+(define (makes-sum? sum number numbers)
+  "Check a given number against all numbers in an input list
+and return #t if it sums to SUM with any of them"
+  (do ((term (car numbers) (car rest))
+       (rest (cdr numbers) (cdr rest)))
+      ((or (= (+ number term) sum) (null? rest))
+       (if (null? rest) #f #t))))
+
+(define (find-pair sum numbers)
+  "Solve the two number sum problem in O(n^2)"
+  (do ((number (car numbers) (car rest))
+       (rest (cdr numbers) (cdr rest)))
+      ((or (makes-sum? sum number numbers) (null? rest))
+       (list number (- sum number)))))
+
+(define (main)
+  (let ((numbers (read-numbers)))
+    (display (reduce * 1 (find-pair 2020 numbers)))))
+
+(main)
---
+++ day01/program-b.scm
@@ -0,0 +1,33 @@
+#!/usr/bin/env guile
+!#
+
+(use-modules (ice-9 rdelim) (srfi srfi-1))
+
+(define (read-numbers)
+  "Read in a list of numbers from stdin"
+  (do ((line (read-line) (read-line))
+       (numbers '() (append numbers (list (string->number line)))))
+      ((eof-object? line) numbers)))
+
+(define (find-pair sum numbers)
+  "Solve the two number sum problem in O(n)"
+  (define diffs (make-hash-table))
+  (do ((number (car numbers) (car rest))
+       (rest (cdr numbers) (cdr rest)))
+      ((or (hash-ref diffs number) (null? rest))
+       (if (hash-ref diffs number) (list number (- sum number)) #f))
+    (hash-set! diffs (- sum number) #t)))
+
+(define (find-triplet sum numbers)
+  "Solve the three number sum problem in O(n^2)"
+  (do ((number (car numbers) (car rest))
+       (rest (cdr numbers) (cdr rest))
+       (pair #f (find-pair (- sum (car rest)) numbers)))
+      ((or pair (null? rest))
+       (if pair (append pair (list number)) #f))))
+
+(define (main)
+  (let ((numbers (read-numbers)))
+    (display (reduce * 1 (find-triplet 2020 numbers)))))
+
+(main)
